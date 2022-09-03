de6100a5bfa8d77838818210fd624fd04ba0e4d8
Ali Zulqarnain <85051992+alizwarraich@users.noreply.github.com>
2022-09-03 22:53:00 +0500

    Delete reference directory

--- reference/fs_demo.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-// fs.mkdir(path.join(__dirname, '/test'), {}, err => { if (err) throw err })
-
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => { if (err) throw err })
